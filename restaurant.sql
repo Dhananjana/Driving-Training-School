ba300a5158dd8fee75aca58e3f8d9efa71255e27
Dhananjana Milinda <dhananjana.milinda.kumarasiri@gmail.com>
2017-10-21 18:41:24 +0530

    admin panel new changes
    Sanju will add the connected to database part here only the view part

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2017 at 09:15 AM
+-- Generation Time: Oct 21, 2017 at 02:52 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
